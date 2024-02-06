f87bd33d70262204fcc8ac1d4882f88889350fd9
WaniaJP <114880028+WaniaJP@users.noreply.github.com>
2024-02-06 15:26:30 +0100

    Etape 4 - L'affichage (pas ouf)

--- src/server.js
+++ src/server.js
@@ -1,3 +1,27 @@
 import {getData} from "./api.js";
-//console.log("Je suis Marvel")
-getData("https://gateway.marvel.com:443/v1/public/characters?")
+import Fastify from 'fastify';
+import fastifyView from '@fastify/view';
+import handlebars from 'handlebars';
+
+const app = Fastify();
+
+app.register(fastifyView, {
+    engine: { handlebars },
+    templates: 'templates',
+    includeViewExtension: true,
+    options: {
+        partials: {
+            header: 'header.hbs',
+            footer: 'footer.hbs'
+        }
+    }
+});
+
+
+app.get('/', async(req,res) => {
+    const characters = await getData("https://gateway.marvel.com:443/v1/public/characters?");
+    return res.view('index.hbs', { comics : characters });
+})
+app.listen({ port: 3000 })
+
+
